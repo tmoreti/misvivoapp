1ded17f2f5aa21a6217dd424ec1d8386ec38c968
tmoreti <thiago.moreti@gmail.com>
2017-02-25 19:45:44 -0300

    novo

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+        "id": "onesignal-cordova-plugin.OneSignal",
+        "pluginId": "onesignal-cordova-plugin",
+        "clobbers": [
+            "OneSignal"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.1",
+    "onesignal-cordova-plugin": "2.0.11"
+}
+// BOTTOM OF METADATA
+});
