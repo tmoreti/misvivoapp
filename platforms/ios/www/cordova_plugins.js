a65750f5bb35b7549fa541f68a8822a8249de02e
tmoreti <thiago.moreti@gmail.com>
2017-02-26 02:14:39 -0300

    opensignal

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,11 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "pushbots-cordova-plugin.PushbotsPlugin",
-        "file": "plugins/pushbots-cordova-plugin/www/pushbots.js",
-        "pluginId": "pushbots-cordova-plugin",
+        "id": "onesignal-cordova-plugin.OneSignal",
+        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+        "pluginId": "onesignal-cordova-plugin",
         "clobbers": [
-            "PushbotsPlugin"
+            "OneSignal"
         ]
     }
 ];
@@ -13,7 +13,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.1",
-    "pushbots-cordova-plugin": "1.4.3"
+    "onesignal-cordova-plugin": "2.0.11"
 };
 // BOTTOM OF METADATA
 });
